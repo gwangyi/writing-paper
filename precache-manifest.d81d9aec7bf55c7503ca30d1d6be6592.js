82557b7e4c200cfb4b81f83545880e4e91a21cc0
Sungkwang Lee <gwangyi.kr@gmail.com>
2020-10-27 22:32:32 +0100

    deploy

---
+++ precache-manifest.d81d9aec7bf55c7503ca30d1d6be6592.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35ad4ce2e67b947b077",
+    "url": "/writing-paper/css/app.488c5f84.css"
+  },
+  {
+    "revision": "b6268b3173a7700dceb495354e04118d",
+    "url": "/writing-paper/img/logo.b6268b31.png"
+  },
+  {
+    "revision": "c778afcaec20063b52ef7638c296a377",
+    "url": "/writing-paper/index.html"
+  },
+  {
+    "revision": "b35ad4ce2e67b947b077",
+    "url": "/writing-paper/js/app.f6929757.js"
+  },
+  {
+    "revision": "096bd8e9b3cc0fd71af9",
+    "url": "/writing-paper/js/chunk-vendors.40ca55ea.js"
+  },
+  {
+    "revision": "323fb3b1741aca37f0055660a767f86b",
+    "url": "/writing-paper/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/writing-paper/robots.txt"
+  }
+]);
